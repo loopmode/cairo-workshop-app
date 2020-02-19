e493342189f565d88026accac5aef78e1f80cc22
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-02-19 09:57:22 +0100

    add redirect from protected route to LoginPage and back

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { useAuthenticated } from "../../hooks/useAuthenticated";
 
 export function ProtectedRoute({ fallback = null, ...props }) {
   const isLoggedIn = useAuthenticated();
   if (!isLoggedIn) {
-    return fallback;
+    if (fallback) {
+      return fallback;
+    }
+    return (
+      <Redirect to={{ pathname: "/login", state: { from: props.location } }} />
+    );
   }
   return <Route {...props} />;
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { LoginForm } from "../components/LoginForm/LoginForm";
+import { useAuthenticated } from "../hooks/useAuthenticated";
+import { useLocation, useHistory } from "react-router-dom";
 
 export const LoginPage = () => {
+  const isAuthenticated = useAuthenticated();
+  const location = useLocation<{ from: string }>();
+  const history = useHistory();
+  const { from } = location.state;
+  React.useEffect(() => {
+    if (isAuthenticated && from) {
+      history.push(from);
+    }
+  }, [history, from, isAuthenticated]);
   return (
     <div>
       <LoginForm />
