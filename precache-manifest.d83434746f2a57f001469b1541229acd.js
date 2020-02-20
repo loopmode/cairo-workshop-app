99b6a55a867ef134f1a0575994a50ec93cba577b
Jovica Aleksic <jovica.aleksic@etecture.de>
2020-02-20 12:26:02 +0100

    Updates

---
+++ precache-manifest.d83434746f2a57f001469b1541229acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a648e999f2e7d111d6b50db8462c8a",
+    "url": "/cairo-workshop-app/index.html"
+  },
+  {
+    "revision": "e86de1ec12df95f7fcd7",
+    "url": "/cairo-workshop-app/static/css/main.e522013e.chunk.css"
+  },
+  {
+    "revision": "d68d0a45b2f2407fbbce",
+    "url": "/cairo-workshop-app/static/js/2.a02b7bf5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/cairo-workshop-app/static/js/2.a02b7bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e86de1ec12df95f7fcd7",
+    "url": "/cairo-workshop-app/static/js/main.ec09a373.chunk.js"
+  },
+  {
+    "revision": "1728c3cdf45e571ebdee",
+    "url": "/cairo-workshop-app/static/js/runtime-main.42dd228b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6162bc904d3b7d235e0e6c9415c29337.js"
+  "/cairo-workshop-app/precache-manifest.d83434746f2a57f001469b1541229acd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cairo-workshop-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
